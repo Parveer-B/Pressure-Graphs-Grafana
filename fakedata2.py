0b315f5649f7fbd9583381b3c75a69fcf143e6ea
Parveer-B <parveer.banwait@gmail.com>
2023-08-25 10:11:17 -0400

    Add uploads folder

--- fakedata2.py
+++ fakedata2.py
@@ -7,11 +7,17 @@ import time
 name = "Data/MassSpecData-04439-" + datetime.strftime(datetime.now(), "%Y%m%d-%H%M%S") + ".csv"
 f = open(name, 'w', newline = "")
 writer = csv.writer(f)
+start = time.time()
 writer.writerow(["</ConfigurationData>"])
 while True:
     f.close()
     time.sleep(1)
     f = open(name, 'a', newline = "")
     writer = csv.writer(f)
-    writer.writerow([datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S.%f"), str(45), str(abs(math.sin(math.pi*int(time.time())/20)))])
-    writer.writerow([datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S.%f"), str(999), str(2 + abs(math.sin(math.pi*int(time.time())/20)))])
+    a = random.random()
+    if a < 0.15:
+        writer.writerow([datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S.%f"), str(998), str(0)])
+    else:
+        writer.writerow([datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S.%f"), str(998), str(abs(math.sin(math.pi*int(time.time())/20)))])
+
+    writer.writerow([datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S.%f"), str(999), str(1.5*(math.e**(-1*(time.time()-start)/8)))])
